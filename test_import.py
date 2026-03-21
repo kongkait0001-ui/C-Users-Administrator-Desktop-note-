2b10a3e536e8437818a8712f0283ec76daf00a51
kongkait0001-ui <kongkait0001@gmail.com>
2026-03-21 16:01:05 +0700

    Delete test_import.py

--- test_import.py
+++
@@ -1,7 +0,0 @@
-try:
-    from streamlit_paste_button import paste_image_button
-    print("Import successful!")
-except ImportError as e:
-    print(f"Import failed: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
